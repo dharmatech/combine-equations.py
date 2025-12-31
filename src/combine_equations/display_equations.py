418ed7c76d37110a9111bef9e1c75922f97bcf06
dharmatech <wayo.cavazos@gmail.com>
2025-12-31 10:48:55 -0800

    updates

--- src/combine_equations/display_equations.py
+++ src/combine_equations/display_equations.py
@@ -5,13 +5,6 @@ def display_equations(equations):
     for eq in equations:
         print(f'{eq.lhs} = {eq.rhs}')
 
-# def display_equations_(equations, want):
-#     for eq in equations:
-#         print(f'{eq.lhs} = {eq.rhs}')
-
-# def display_equation_(eq, want):
-#     print(f'{eq.lhs} = {eq.rhs}')
-
 def red_text(s):
     RED = "\x1b[31m"
     RESET = "\x1b[0m"
@@ -21,26 +14,6 @@ def green_text(s):
     GREEN = "\x1b[32m"
     RESET = "\x1b[0m"
     return f"{GREEN}{s}{RESET}"
-
-# def display_equation_(eq, want):
-#     colored_want = sp.Symbol(red_text(str(want)))
-#     eq = eq.subs({want: colored_want})
-#     print(f'{eq.lhs} = {eq.rhs}')
-
-# def display_equation_(eq, values, want):
-
-#     colored_want = sp.Symbol(red_text(str(want)))
-
-#     eq = eq.subs({want: colored_want})
-    
-#     colored_knowns = {}
-
-#     for sym in values.keys():
-#         colored_knowns[sym] = sp.Symbol(green_text(str(sym)))
-
-#     eq = eq.subs(colored_knowns)
-
-#     print(f'{eq.lhs} = {eq.rhs}')
 
 def display_equation_(eq, values=None, want=None):
 

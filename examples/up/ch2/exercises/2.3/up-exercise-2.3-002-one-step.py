4700697b1383c945e35bd99f0dcaa8f579a96870
dharmatech <wayo.cavazos@gmail.com>
2026-01-27 10:12:00 -0800

    updates

---
+++ examples/up/ch2/exercises/2.3/up-exercise-2.3-002-one-step.py
@@ -0,0 +1,62 @@
+
+# Trip Home.
+# 
+# You normally drive on the freeway between San Diego and Los Angeles 
+# at an average speed of 105 km/h (65 mi/h),
+# and the trip takes 1 h and 50 min.
+# 
+# On a Friday afternoon, however, heavy traffic slows you down 
+# and you drive the same distance 
+# at an average speed of only 70 km/h (43 mi/h).
+# 
+# How much longer does the trip take?
+# ----------------------------------------------------------------------
+from pprint import pprint
+import sympy as sp
+
+from sympy.physics.units import newton, meter, second, meters
+from sympy.physics.units import N, m, s, km, h
+
+from combine_equations.kinematics_states import *
+from combine_equations.solve_system import *
+from combine_equations.display_equations import display_equations_
+from combine_equations.solve_and_display import solve_and_display_
+from combine_equations.misc import eq_flat
+from combine_equations.newtons_laws import *
+# ----------------------------------------------------------------------
+ca = make_states_model("ca", 2)  # car trip normal
+cb = make_states_model("cb", 2)  # car trip slow
+
+ca0, ca1 = ca.states
+cb0, cb1 = cb.states
+
+ca01 = ca.edges[0]
+cb01 = cb.edges[0]
+
+eqs = []
+
+eqs += kinematics_fundamental(ca, axes=['x'])
+eqs += kinematics_fundamental(cb, axes=['x'])
+
+eqs += eq_flat(
+    ca1.pos.x,
+    cb1.pos.x,
+)
+# ----------------------------------------------------------------------
+
+values = {}
+
+values[ca0.t] = 0 * h
+values[ca1.t] = 1 * h + 50/60 * h
+values[ca0.pos.x] = 0 * km
+values[ca01.v_av.x] = 105 * km / h
+
+values[cb0.t] = 0 * h
+values[cb0.pos.x] = 0 * km
+values[cb01.v_av.x] = 70 * km / h
+
+display_equations_(eqs, values, cb1.t)
+solve_and_display_(eqs, values, want=cb1.t)
+# cb_1_t = (-ca_0_t*v_av_x_ca_0_1 + ca_0_x + ca_1_t*v_av_x_ca_0_1 + cb_0_t*v_av_x_cb_0_1 - cb_0_x)/v_av_x_cb_0_1
+# cb_1_t = 2.75*hour
+
--- src/combine_equations/solve_and_display.py
+++ src/combine_equations/solve_and_display.py
@@ -3,7 +3,7 @@ import sympy as sp
 from combine_equations.solve_system import *
 from combine_equations.display_equations import display_equation_
 
-def solve_and_display_(equations, values, want, version=1):
+def solve_and_display_(equations, values, want, version=1, return_solutions=False):
     
     if version == 1:
         tmp = solve_system_multiple_solutions(equations, values, want)
@@ -20,3 +20,15 @@ def solve_and_display_(equations, values, want, version=1):
         display_equation_(sol, values, want=want)
         # display_equation_(sol.subs(values), values, want=want)
         display_equation_(sp.N(sol.subs(values)), values, want=want)
+        
+    if return_solutions:
+
+        solutions = []
+
+        for sol in tmp:
+            symbolic = sol
+            numeric = sp.N(sol.subs(values))
+            solutions.append([symbolic, numeric])
+
+        return solutions
+
